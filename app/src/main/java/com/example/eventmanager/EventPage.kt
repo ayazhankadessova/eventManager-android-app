6127a9b830290a1fa1817252d9126108f229b12e
ayazhankadessova <kadessovaayazhan@gmail.com>
2024-04-01 06:09:45 +0800

    add: one event on page working (w/o Join Event)

--- app/src/main/java/com/example/eventmanager/EventPage.kt
+++ app/src/main/java/com/example/eventmanager/EventPage.kt
@@ -1,6 +1,5 @@
 package com.example.eventmanager
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
@@ -8,19 +7,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -44,6 +38,49 @@ fun EventPage(event: Event) {
                         modifier = Modifier
                             .fillMaxWidth()
                             .aspectRatio(1f)
+                    )
+
+                }
+            }
+
+            Box(modifier = Modifier.fillMaxWidth().padding(12.dp)) {
+                Column{
+                    Text(
+                        text =event.title,
+                        style = TextStyle(fontSize = 27.sp),
+                        modifier = Modifier.padding(top = 5.dp, bottom = 5.dp)
+
+                    )
+                    Text(
+                        text = event.organiser,
+                        style = TextStyle(fontSize = 18.sp, color = Color.Gray),
+                        modifier = Modifier.padding(bottom = 5.dp)
+                    )
+                    Text(text=event.description, style = TextStyle(fontSize = 20.sp), modifier = Modifier.padding(bottom = 16.dp))
+
+                    Divider()
+
+                    Text(
+                        text = "Date: " + event.event_date,
+                        modifier = Modifier.padding(top = 16.dp, bottom = 16.dp),
+                        style = TextStyle(fontSize = 20.sp)
+
+                    )
+
+                    Divider()
+
+                    Text(
+                        text = "Location: " + event.location,
+                        modifier = Modifier.padding(top = 16.dp, bottom = 16.dp),
+                        style = TextStyle(fontSize = 20.sp)
+                    )
+
+                    Divider()
+
+                    Text(
+                        text = "Quota: " + event.quota.toString(),
+                        modifier = Modifier.padding(top = 16.dp, bottom = 16.dp),
+                        style = TextStyle(fontSize = 20.sp)
                     )
                 }
             }
